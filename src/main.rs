0983d25b4d7e274ba24ac1075873f8157fc54a09
xd009642 <danielmckenna93@gmail.com>
2021-10-21 19:13:47 +0100

    Fill in what needs to be done

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-use cargo_tarpaulin::config::*;
 use cargo_tarpaulin::source_analysis::*;
+use cargo_tarpaulin::config::*;
 use std::path::PathBuf;
 
 fn main() {
     let mut config = Config::default();
-    config.manifest = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-        .join("test-project")
-        .join("Cargo.toml");
+    config.manifest = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("test-project").join("Cargo.toml");
     println!("Manifest: {}", config.manifest.display());
+
+    let analysis = SourceAnalysis::get_analysis(&config);
+
+    // TODO Build the test binary 
+    
+    // TODO get the lines we can get coverage for 
+
+    // TODO Program board with test binary
+
+    // TODO Use probe-rs to collect coverage info
 }
